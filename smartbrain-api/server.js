4213bcdd4507b862dcc8327f5d1b9cba38604adc
Krzysztof <cristopherb95@yahoo.com>
2020-01-25 21:22:36 +0100

    Setting up a server

---
+++ smartbrain-api/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'john',
+            email: 'john@mail.com',
+            password: '123qwe',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'sally',
+            email: 'sally@mail.com',
+            password: '123qwe',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('Success!')
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    });
+    res.json(database.users[database.users.length - 1]);
+})
+
+app.listen(4000, () => {
+    console.log('Server has started on port 4000!');
+});
+
